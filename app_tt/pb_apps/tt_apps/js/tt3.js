7a24585b12e1f011685ddac47e5eb17c7d12019a
jeymisson <jeymissoncz@gmail.com>
2013-02-25 16:39:08 -0300

    added tt3 add line/column option

--- app_tt/pb_apps/tt_apps/js/tt3.js
+++ app_tt/pb_apps/tt_apps/js/tt3.js
@@ -58,7 +58,6 @@ function salvarAlteracoes(){
             }
         }
     }
-    return resultado;
 }
 
 function isBorda(pontos){
@@ -249,6 +248,211 @@ function verificaExistenciaLinha(pontos){
     return false;
 }
 
+function adicionarNovaLinha(){
+    var posY = Math.round((maxY - minY) / 2);
+    while (temLinha(posY) && posY < maxY) {
+        posY += 5;
+    }
+    var intercessoes = encontraIntercessoesVerticais(posY);
+    for (var z=0; z<intercessoes.length - 1; z++){
+        var pontos = [intercessoes[z], posY, intercessoes[z+1], posY];
+        adicionarLinha(pontos);
+        atualizarIntercessao(pontos);
+    }
+}
+
+function adicionarNovaColuna(){
+    var posX = Math.round((maxX - minX) / 2);
+    while (temLinha(posX) && posX < maxX) {
+        posX += 5;
+    }
+    var intercessoes = encontraIntercessoesHorizontais(posX);
+    for (var z=0; z<intercessoes.length - 1; z++){
+        var pontos = [posX, intercessoes[z], posX, intercessoes[z+1]];
+        adicionarLinha(pontos);
+        atualizarIntercessao(pontos);
+    }
+}
+
+function temLinha(posY){
+    var pts2;
+    for (var z = 0; z<linhas.length; z++){
+        pts2 = linhas[z].getPoints();
+        if (pts2[0].y == pts2[1].y && pts2[0].y == posY) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function atualizarIntercessao(pontos){
+    var pts2;
+    var aAdicionar = new Array();
+    for (var z=0; z<linhas.length; z++){
+        pts2 = linhas[z].getPoints();
+        if (pontos[1] == pontos[3]){
+            if (pts2[0].x == pts2[1].x){ 
+                if (pontos[0] <= pts2[0].x && pontos[2] >= pts2[1].x && pts2[0].y < pontos[1] && pts2[1].y > pontos[1]){
+                    linhas[z].setPoints([pts2[0].x, pts2[0].y, pts2[1].x, pontos[1]]);
+                    aAdicionar.push([pts2[0].x, pontos[1], pts2[1].x, pts2[1].y]);
+                }
+            }
+        }
+        else if (pontos[0] == pontos[2]){
+            if (pts2[0].y == pts2[1].y){ 
+                if (pontos[1] <= pts2[0].y && pontos[3] >= pts2[1].y && pts2[0].x < pontos[0] && pts2[1].x > pontos[2]){
+                    linhas[z].setPoints([pts2[0].x, pts2[0].y, pontos[0], pts2[1].y]);
+                    aAdicionar.push([pontos[0], pts2[0].y, pts2[1].x, pts2[1].y]);
+                }
+            }
+        }
+    }
+    for (var w=0; w < aAdicionar.length; w++){
+        adicionarLinha(aAdicionar[w]);
+    }
+}
+
+function encontraIntercessoesVerticais(altura) {
+    var intercessoes = new Array();
+    for (var z=0; z<linhas.length; z++){
+        var pts2 = linhas[z].getPoints();
+        if(pts2[0].x == pts2[1].x) {
+            if (pts2[0].y <= altura && pts2[1].y >= altura) {
+                if (($.inArray(pts2[0].x, intercessoes)) == -1){
+                    intercessoes.push(pts2[0].x);
+                }
+            }
+        }
+    }
+    intercessoes.sort(function(a,b){
+        return a - b
+    });
+    return intercessoes;
+}
+
+function encontraIntercessoesHorizontais(distancia) {
+    var intercessoes = new Array();
+    for (var z=0; z<linhas.length; z++){
+        var pts2 = linhas[z].getPoints();
+        if(pts2[0].y == pts2[1].y) {
+            if (pts2[0].x <= distancia && pts2[1].x >= distancia) {
+                if (($.inArray(pts2[0].y, intercessoes)) == -1){
+                    intercessoes.push(pts2[0].y);
+                }
+            }
+        }
+    }
+    intercessoes.sort(function(a,b){
+        return a - b
+    });
+    return intercessoes;
+}
+
+function adicionarLinha(pontos){
+    var linha = new Kinetic.Line({
+        points : pontos,
+        stroke : 'red',
+        draggable : false,
+        strokeWidth : 2,
+        dragBoundFunc: function(pos, event) {
+            var points = this.getPoints();
+            if (points[0].x == points[1].x){
+                var posX = event.offsetX==undefined?event.layerX:event.offsetX;
+                var limEsq = encontraLimiteEsquerda(points[0].x);
+                var limDir = encontraLimiteDireita(points[0].x);
+                var newX = posX < limEsq ? limEsq : (posX > limDir ? limDir : posX);
+                if (movendoLinha) {
+                    if(isColunaMovivel(newX)){
+                        moverColuna(points[0].x, newX);
+                    }
+                }
+            } else {
+                var posY = event.offsetY==undefined?event.layerY:event.offsetY;
+                var limSup = encontraLimiteSuperior(points[0].y);
+                var limInf = encontraLimiteInferior(points[0].y);
+                var newY = posY < limSup ? limSup: (posY > limInf ? limInf : posY);
+                if (movendoLinha) {
+                    if (isLinhaMovivel(newY)){
+                        moverLinha(points[0].y, newY);
+                    }
+                }
+            }
+
+            return {
+                x: 0,
+                y: 0
+            }
+        }
+    });
+    var pts= linha.getPoints();
+    if (pts[0].x == pts[1].x) {
+        linha.on("mouseover", function() {
+            if (movendoLinha) {
+                document.body.style.cursor = "ew-resize";
+            } else if (removendo || removendoLinha) {
+                document.body.style.cursor = "pointer";
+                layer.draw();
+            }
+        });
+    } else {
+        linha.on("mouseover", function() {
+            if (movendoLinha) {
+                document.body.style.cursor = "ns-resize";
+            } else if (removendo || removendoLinha) {
+                document.body.style.cursor = "pointer";
+                layer.draw();
+            }
+        });
+    }
+
+    linha.on("mouseout", function() {
+        document.body.style.cursor = "default";
+        layer.draw();
+    });
+
+    linha.on("mouseover touchstart", function() {
+        if (movendoLinha || removendoLinha) {
+            var points = this.getPoints();
+            if (points[0].x == points[1].x) {
+                var z;
+                for (z = 0; z < linhas.length; z++) {
+                    var points2 = linhas[z].getPoints();
+                    if (points2[0].x == points2[1].x
+                            && points[0].x == points2[0].x) {
+                        linhas[z].setStroke('green');
+                    }
+                }
+            } else if (points[0].y == points[1].y) {
+                var z;
+                for (z = 0; z < linhas.length; z++) {
+                    var points2 = linhas[z].getPoints();
+                    if (points2[0].y == points2[1].y
+                            && points[0].y == points2[0].y) {
+                        linhas[z].setStroke('green');
+                    }
+                }
+            }
+        } else {
+            this.setStroke('green');
+        }
+
+        if (removendo || removendoLinha) {
+            this.setDraggable(false);
+        }
+        layer.draw();
+    });
+    linha.on('mouseout touchend', function() {
+        for (i = 0; i < linhas.length; i++) {
+            linhas[i].setStroke('red');
+        }
+        this.setDraggable(true);
+        layer.draw();
+    });
+    linhas.push(linha);
+    layer.add(linha);
+    stage.add(layer);
+}
+
 $(document).ready(function() {
 
     $("#remover").click(function() {
@@ -271,13 +475,81 @@ $(document).ready(function() {
     });
 });
 
+function encontraLimiteSuperior(posY){
+    var posicoesY = new Array();
+    var pts2;
+    for (var z=0; z<linhas.length; z++){
+        pts2 = linhas[z].getPoints();
+        if (pts2[0].y == pts2[1].y && pts2[0].y < posY){
+            posicoesY.push(pts2[0].y);
+        }
+    } if (posicoesY.length == 0){
+        return minY;
+    } 
+    posicoesY.sort(function(a,b){
+        return b - a;
+    });
+    return posicoesY[0];
+}
+
+function encontraLimiteInferior(posY){
+    var posicoesY = new Array();
+    var pts2;
+    for (var z=0; z<linhas.length; z++){
+        pts2 = linhas[z].getPoints();
+        if (pts2[0].y == pts2[1].y && pts2[0].y > posY){
+            posicoesY.push(pts2[0].y);
+        }
+    } if (posicoesY.length == 0){
+        return maxY;
+    } 
+    posicoesY.sort(function(a,b){
+        return a - b;
+    });
+    return posicoesY[0];
+}
+
+function encontraLimiteEsquerda(posX){
+    var posicoesX = new Array();
+    var pts2;
+    for (var z=0; z<linhas.length; z++){
+        pts2 = linhas[z].getPoints();
+        if (pts2[0].x == pts2[1].x && pts2[0].x < posX){
+            posicoesX.push(pts2[0].x);
+        }
+    } if (posicoesX.length == 0){
+        return minX;
+    } 
+    posicoesX.sort(function(a,b){
+        return b - a;
+    });
+    return posicoesX[0];
+}
+
+function encontraLimiteDireita(posX){
+    var posicoesX = new Array();
+    var pts2;
+    for (var z=0; z<linhas.length; z++){
+        pts2 = linhas[z].getPoints();
+        if (pts2[0].x == pts2[1].x && pts2[0].x > posX){
+            posicoesX.push(pts2[0].x);
+        }
+    } if (posicoesX.length == 0){
+        return maxX;
+    } 
+    posicoesX.sort(function(a,b){
+        return a - b;
+    });
+    return posicoesX[0];
+}
+
 function initGrid(matrizDePontos, uri) {
     minX = 2000;
     minY = 2000;
     maxX = 0;
     maxY = 0;
                 
-    for (i = 1; i < matrizDePontos.length; i++) {
+    for (i = 0; i < matrizDePontos.length; i++) {
         arr = matrizDePontos[i];
         
         leftX = arr[0];
@@ -297,117 +569,21 @@ function initGrid(matrizDePontos, uri) {
 
         for (var z=0; z<novasLinhas.length; z++){
             if (!verificaExistenciaLinha(novasLinhas[z])){
-                var linha = new Kinetic.Line({
-                    points : novasLinhas[z],
-                    stroke : 'red',
-                    draggable : false,
-                    strokeWidth : 2,
-                    dragBoundFunc: function(pos, event) {
-                        var points = this.getPoints();
-                        if (points[0].x == points[1].x){
-                            var posX = event.offsetX==undefined?event.layerX:event.offsetX;
-                            var newX = posX < minX ? minX : (posX > maxX ? maxX : posX);
-                            if (movendoLinha) {
-                                if(isColunaMovivel(newX)){
-                                    moverColuna(points[0].x, newX);
-                                }
-                            }
-                        } else {
-                            var posY = event.offsetY==undefined?event.layerY:event.offsetY;
-                            var newY = posY < minY ? minY : (posY > maxY ? maxY : posY);
-                            if (movendoLinha) {
-                                if (isLinhaMovivel(newY)){
-                                    moverLinha(points[0].y, newY);
-                                }
-                            }
-                        }
-
-                        return {
-                            x: 0,
-                            y: 0
-                        }
-                    }
-                });
-                var pts= linha.getPoints();
-                if (pts[0].x == pts[1].x) {
-                    linha.on("mouseover", function() {
-                        if (movendoLinha) {
-                            document.body.style.cursor = "ew-resize";
-                        } else if (removendo || removendoLinha) {
-                            document.body.style.cursor = "pointer";
-                            layer.draw();
-                        }
-                    });
-                } else {
-                    linha.on("mouseover", function() {
-                        if (movendoLinha) {
-                            document.body.style.cursor = "ns-resize";
-                        } else if (removendo || removendoLinha) {
-                            document.body.style.cursor = "pointer";
-                            layer.draw();
-                        }
-                    });
-                }
-
-                linha.on("mouseout", function() {
-                    document.body.style.cursor = "default";
-                    layer.draw();
-                });
-
-                linha.on("mouseover touchstart", function() {
-                    if (movendoLinha || removendoLinha) {
-                        var points = this.getPoints();
-                        if (points[0].x == points[1].x) {
-                            var z;
-                            for (z = 0; z < linhas.length; z++) {
-                                var points2 = linhas[z].getPoints();
-                                if (points2[0].x == points2[1].x
-                                        && points[0].x == points2[0].x) {
-                                    linhas[z].setStroke('green');
-                                }
-                            }
-                        } else if (points[0].y == points[1].y) {
-                            var z;
-                            for (z = 0; z < linhas.length; z++) {
-                                var points2 = linhas[z].getPoints();
-                                if (points2[0].y == points2[1].y
-                                        && points[0].y == points2[0].y) {
-                                    linhas[z].setStroke('green');
-                                }
-                            }
-                        }
-                    } else {
-                        this.setStroke('green');
-                    }
-
-                    if (removendo || removendoLinha) {
-                        this.setDraggable(false);
-                    }
-                    layer.draw();
-                });
-                linha.on('mouseout touchend', function() {
-                    for (i = 0; i < linhas.length; i++) {
-                        linhas[i].setStroke('red');
-                    }
-                    this.setDraggable(true);
-                    layer.draw();
-                });
-                linhas.push(linha);
-                layer.add(linha);
+                adicionarLinha(novasLinhas[z]);
             }
         }
     }
 
     layer.on('click', function(evt) {
 
-        //C�digo para remover apenas um segmento da tabela
+        //Codigo para remover apenas um segmento da tabela
         if (removendo) {
             
             // Recupera o objeto que foi clicado
             var shape = evt.shape;
             var points = shape.getPoints();
 
-            // � necess�rio fazer itera��o inversa sobre o array para n�o pular nenhum elemento.
+            // E necessario fazer iteracao inversa sobre o array para nao pular nenhum elemento.
             for (i = linhas.length - 1; i >= 0; i--) {
                 var points2 = linhas[i].getPoints();
                 if (points2[0].x == points[0].x && 
@@ -420,7 +596,7 @@ function initGrid(matrizDePontos, uri) {
                 }
             }
         }
-        //C�digo para remover todos os segmentos que perten�am a uma mesma linha
+        //Codigo para remover todos os segmentos que pertencam a uma mesma linha
         else if (removendoLinha) {
 
             // Recupera o objeto que foi clicado
@@ -428,7 +604,7 @@ function initGrid(matrizDePontos, uri) {
             var points = shape.getPoints();
             if (points[0].x == points[1].x) {
 
-                // � necess�rio fazer itera��o inversa sobre o array para n�o pular nenhum elemento.
+                // E necessario fazer iteracao inversa sobre o array para nao pular nenhum elemento.
                 for (i = linhas.length - 1; i >= 0; i--) {
                     var points2 = linhas[i].getPoints();
                     if (points2[0].x == points[0].x
@@ -448,7 +624,7 @@ function initGrid(matrizDePontos, uri) {
                 }
             }
         }
-        // Ap�s remover os objetos selecionados, redesenhamos a camada apenas com os objetos restantes.
+        // Apos remover os objetos selecionados, redesenhamos a camada apenas com os objetos restantes.
         layer.draw();
     });
     var imageObj = new Image();
