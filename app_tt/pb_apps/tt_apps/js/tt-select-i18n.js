766c64dc7ba2ed64c2c18525fe820d07a91c2111
jeymisson <jeymissoncz@gmail.com>
2013-05-31 14:45:24 -0300

    added progress bar to tt1

--- app_tt/pb_apps/tt_apps/js/tt-select-i18n.js
+++ app_tt/pb_apps/tt_apps/js/tt-select-i18n.js
@@ -4,7 +4,6 @@ var default_dict = {
     'button_no':  "No",
     'button_ntk': "I don't know",
     'task_name': "Task: ",
-    'well_done': "Well done! ",
     'saved': "Your answer has been saved",
     'congratulations': "Congratulations! ",
     'finished': "All the tasks have been completed!",
@@ -12,7 +11,9 @@ var default_dict = {
     'other_apps': "or, Check other applications",
     'error': "Error! ",
     'err_msg': "Something went wrong, please contact the site administrators.",
-    'workflowtask' : "There is a new kind of task available. Click to Try it."
+    'workflowtask' : "There is a new kind of task available. Click to Try it.",
+    'progress-from' : "You have completed:",
+    'progress-to' : "tasks from"
 }
 
 var en_US_dict = default_dict;
@@ -23,7 +24,6 @@ var pt_dict= {
 	    'button_no':  "Não",
 	    'button_ntk': "Não sei",
 	    'task_name': "Tarefa: ",
-	    'well_done': "Muito bem! ",
 	    'saved': "Sua resposta foi salva",
 	    'congratulations': "Parabéns! ",
 	    'finished': "Todas as tarefas foram finalizadas",
@@ -31,8 +31,9 @@ var pt_dict= {
 	    'other_apps': "ou, verificar outras aplicações",
 	    'error': "Erro! ",
 	    'err_msg': "Algo ocorreu errado, por favor contate o administrador do site.",
-        'workflowtask' : "Existe um novo tipo de tarefa disponível. Clique para ir."
-		
+        'workflowtask' : "Existe um novo tipo de tarefa disponível. Clique para ir.",
+        'progress-from' : "Você está na tarefa:",
+        'progress-to' : "de"
 }
 
 var language = navigator.language? navigator.language : navigator.userLanguage;
