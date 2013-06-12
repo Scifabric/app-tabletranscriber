dab04e20e9363b0eebadfe03c06d6edea7097467
jeymisson <jeymissoncz@gmail.com>
2013-06-12 14:31:28 -0300

    Added progress bar at collaborate page

--- app_tt/view/collaborate.py
+++ app_tt/view/collaborate.py
@@ -1,9 +1,13 @@
+# -*- coding:utf-8 -*-
+
 import urllib2
 import json
 
 from app_tt.core import app as flask_app
+from app_tt.core import pbclient
 from flask import Blueprint, render_template, request
 from app_tt.util import Pagination
+import sys
 import requests
 
 
@@ -11,6 +15,7 @@ blueprint = Blueprint('collaborate', __name__)
 pybossa_server = flask_app.config['PYBOSSA_URL']
 
 api_key = flask_app.config['API_KEY']
+max_limit = sys.maxint
 
 
 def getAppData(app_short_name, pybossa_server):
@@ -20,14 +25,14 @@ def getAppData(app_short_name, pybossa_server):
 
 def getAppTasks(app_id, pybossa_server):
     return json.load(urllib2.urlopen(pybossa_server + '/api/task?app_id=%d&limit=%d' %
-            (app_id, 10**7)))
+            (app_id, max_limit)))
 
 
 @blueprint.route('/', defaults={'page': 1})
 @blueprint.route('/page/<int:page>')
 def index(page):
     per_page = 5
-    apps = json.loads(requests.get(pybossa_server + '/api/app?api_key=%s&limit=%d' % (api_key, 10**7)).content)
+    apps = json.loads(requests.get(pybossa_server + '/api/app?api_key=%s&limit=%d' % (api_key, max_limit)).content)
     book_stack = []
     book_data = []
 
@@ -46,20 +51,44 @@ def index(page):
 
     count = len(book_data)
     books = []
-    for index in range((page - 1) * per_page, page * per_page ):
+    for index in range((page - 1) * per_page, page * per_page):
         if(index < count):
             books.append(book_data[index])
 
-    if(not book_data and page!=1):
-        abort(404)
+    if(not book_data and page != 1):
+        error = "Oops! Essa página não existe, contate o administrador"
+        return render_template('/error.html', error=error)
 
     pagination = Pagination(page, per_page, count)
-
 
     return render_template('/collaborate/index.html',
             books=books,
             pagination=pagination)
 
+
+@blueprint.route('/progress/<string(maxlength=255):bookid>')
+def progress(bookid):
+
+    tt_suffix = ['_tt1', '_tt2', '_tt3']  #TODO: Add tt4 when it's ready
+    overall = 0
+    overall_completed = 0
+    for suffix in tt_suffix:
+        completed = 0
+        app = pbclient.find_app(short_name=str(bookid) + suffix)[0]
+        tasks = pbclient.get_tasks(app.id, limit=max_limit)
+        overall += len(tasks)
+        for task in tasks:
+            if task.state == 'completed':
+                completed += 1
+        
+        overall_completed += completed
+
+    try:
+        progress = overall_completed/float(overall)
+    except:
+        return render_template('/progress.html', progress=0)
+
+    return render_template('/progress.html', progress=progress)
 
 
 @blueprint.route('/book', methods=['GET'])
@@ -81,8 +110,7 @@ def book():
                 "img_url": str(i+1),
                 "tasks": tasks,
                 "finished": [task for task in tasks if task["state"] == "completed"],
-                "url": pybossa_server + "/app/" + app_uri}
-                )
+                "url": pybossa_server + "/app/" + app_uri})
 
     if(apps):
         app_tasks = apps
@@ -94,4 +122,3 @@ def book():
     return render_template('/collaborate/book.html',
             bookid=bookid_app,
             appTasks=app_tasks)
-
