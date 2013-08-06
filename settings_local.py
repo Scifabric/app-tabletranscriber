d807f8c0b00c63f53329fd91f207404b0ecf985c
Adabriand <adabriand@gmail.com>
2013-08-06 17:57:10 -0300

    undo commit of app_tt.wsgi and settings_local.py

--- settings_local.py
+++ settings_local.py
@@ -4,12 +4,12 @@ URL_TEMPLATES = "http://localhost/mb-static" #Static files url
 PYBOSSA_HOST = "127.0.0.1" #Pybossa's address
 PYBOSSA_PORT = 80  #Pybossa's server
 PYBOSSA_ENDPOINT = "/pybossa" #Pybossa's endpoint, found after pybossa's port number
-API_KEY = "b9dea9c6-ea8f-4420-b731-2cbaff8e95de" # Pybossa's Api Key
+API_KEY = "a9b643e2-fdd0-4d57-907e-2efcfe6205cf" # Pybossa's Api Key
 
 # RabbitMQ definitions
-RABBIT_VHOST = "mb-vhost"
-RABBIT_USER = "mb"
-RABBIT_PASSWD = "mb"
+RABBIT_VHOST = "myvhost"
+RABBIT_USER = "mbuser"
+RABBIT_PASSWD = "mbuser"
 
 #MB database definitions
 DB_USER = "mbuser"
