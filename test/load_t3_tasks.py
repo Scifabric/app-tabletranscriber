13845e29ea7237dcfcebff4730c5cfad47ac7131
Adabriand <adabriand@gmail.com>
2013-09-16 17:05:03 -0300

    Loader script for T3 task

---
+++ test/load_t3_tasks.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import psycopg2
+import psycopg2.extras
+import sys
+import urllib2
+
+if len(sys.argv) != 6:
+	print("Usage load_t3_tasks.py <db host> <db name> <db user> <db pass> <app id>")
+	sys.exit(1)
+
+con = None
+
+try:
+    
+    conn_string = "host='"+ sys.argv[1] + "' dbname='" + sys.argv[2] + "' user='" + sys.argv[3] + "' password='" + sys.argv[4] + "'" 
+    con = psycopg2.connect(conn_string) 
+    cursor = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute("SELECT id FROM task WHERE app_id = " +  sys.argv[5] + " and state = 'completed'")
+    records = cursor.fetchall()
+
+    for i in records:
+	request = "http://localhost/mb/api/" + str(i["id"]) + "/done"
+	print("Task ID: " + str(i["id"]) + " Created: " + urllib2.urlopen(request).read())
+
+except psycopg2.DatabaseError, e:
+    print 'Error %s' % e    
+    sys.exit(1)
+    
+    
+finally:
+    
+    if con:
+        con.close()
