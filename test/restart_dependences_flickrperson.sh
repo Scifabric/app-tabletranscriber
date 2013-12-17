5212364de995b0d794541f2d3e18c4c42180af56
guilhermemg <guilhermemgadelha@gmail.com>
2013-12-17 17:24:15 -0300

    fix_links_socientize.py file

---
+++ test/restart_dependences_flickrperson.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ..
+
+#reload templates
+. env/bin/activate
+python ../app-flickrperson/createTasks.py -s http://localhost/pybossa -k 9134c60c-1caa-4935-8eb0-15f3527809de -c
+python ../app-flickrperson/createTasks.py -t ../app-flickrperson/static/fb-templates/template.html
+deactivate
+
+sudo service apache2 restart
