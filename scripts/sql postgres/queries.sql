8286283b343f3063ca51585d2b05ba1f4d08d49a
guilhermemg <guilhermemgadelha@gmail.com>
2014-03-17 15:58:08 -0300

    new scritps to restart dependences

---
+++ scripts/sql postgres/queries.sql
@@ -0,0 +1,4 @@
+select info from task_run where app_id in (select id from app where short_name like '%anuario1916pb%')
+
+--delete from task where app_id in (select id from app where short_name like '%anuario1916pb%')
+delete from app where short_name like '%anuario1916pb%'
